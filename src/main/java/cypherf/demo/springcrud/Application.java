308839d5211aaea584f6f4b6f1c3f652892eb191
Cypher Firelair <gaetan.l@protonmail.com>
2023-09-26 10:33:54 +0200

    Replacing Application calls to MessageRepository with MessageService

--- src/main/java/cypherf/demo/springcrud/Application.java
+++ src/main/java/cypherf/demo/springcrud/Application.java
@@ -1,7 +1,7 @@
 package cypherf.demo.springcrud;
 
 import cypherf.demo.springcrud.model.Message;
-import cypherf.demo.springcrud.repository.MessageRepository;
+import cypherf.demo.springcrud.service.MessageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +11,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
 
 /**
  * {@literal @}ComponentScan scans for {@literal @}Bean, {@literal @}Component,
@@ -27,7 +25,7 @@ import java.util.Optional;
 @EnableJpaRepositories
 public class Application {
 	@Autowired
-	private MessageRepository messageRepository;
+	private MessageService messageService;
 
 	final static Logger logger = LoggerFactory.getLogger(Application.class);
 
@@ -39,102 +37,60 @@ public class Application {
 
 	public void run(String... args) {
 		// SAVE
-		logger.debug("saving message 1/one...");
-		Message messageOne = messageRepository.save(new Message("Message one"));
+		logger.info("creating message 1/one...");
+		Message messageOne = messageService.create("Message one");
 
-		logger.debug("saving message 2/two...");
-		Message messageTwo = messageRepository.save(new Message("Message two"));
+		logger.info("creating message 2/two...");
+		Message messageTwo = messageService.create("Message two");
 
-		// SAVE ALL
-		logger.debug("saving messages 3/three to 8/eight...");
-		Message messageThree = new Message("Message three");
-		Message messageFour = new Message("Message four");
-		Message messageFive = new Message("Message five");
-		Message messageSix = new Message("Message six");
-		Message messageSeven = new Message("Message seven");
-		Message messageEight = new Message("Message eight");
-		List<Message> messages = Arrays.asList(messageThree, messageFour, messageFive, messageSix, messageSeven, messageEight);
-		messageRepository.saveAll(messages);
+		logger.info("creating message 3/three...");
+		Message messageThree = messageService.create("Message three");
+
+		logger.info("creating message 4/four...");
+		Message messageFour = messageService.create("Message four");
+
+		logger.info("creating message 5/five...");
+		Message messageFive = messageService.create("Message five");
+
+		logger.info("creating message 6/six...");
+		Message messageSix = messageService.create("Message six");
+
+		logger.info("creating message 7/seven...");
+		Message messageSeven = messageService.create("Message seven");
+
+		logger.info("creating message 8/eight...");
+		Message messageEight = messageService.create("Message eight");
 
 		// COUNT
-		logger.debug("counting messages...");
-		logger.debug("Number of messages: " + messageRepository.count());
+		logger.info("counting messages...");
+		logger.info("Number of messages: " + messageService.readAll().size());
 
-		// EXISTS
-		long idFake = 0;
-		logger.debug("checking if message by id=" + idFake + " exists...");
-		boolean exists = messageRepository.existsById(idFake);
-		logger.debug("Message with id=" + idFake + (exists ? " exists" : " doesn't exist"));
-
-		long idOne = messageOne.getId();
-		logger.debug("checking if message by id=" + idOne + " exists...");
-		exists = messageRepository.existsById(idOne);
-		logger.debug("Message with id=" + idOne + (exists ? " exists" : " doesn't exist"));
-
-		// FIND BY ID
-		logger.debug("finding message by id=" + idOne + "...");
-		Optional<Message> dbMessageOne = messageRepository.findById(idOne);
-		if (dbMessageOne.isEmpty()) {
-			logger.debug("Message with id=" + idOne + " not found");
-		}
-		else {
-			logger.debug("Message with id=" + idOne + " found: " + dbMessageOne.get());
-		}
-
-		// FIND ALL BY ID
-		long idTwo = messageTwo.getId();
-		List<Long> ids = Arrays.asList(idOne, idTwo);
-		logger.debug("finding messages by ids=" + ids + "...");
-		messageRepository.findAllById(ids).forEach(m -> {
-			logger.debug("Message: " + m.toString());
+		// READ BY ID
+		Arrays.asList(0L, messageOne.getId()).forEach(id -> {
+			logger.info("checking if message with id=" + id + " exists...");
+			logger.info("Message with id=" + id + (messageService.readById(id).map(message -> " found: " + message).orElse(" not found")));
 		});
 
 		// FIND ALL
 		printAllMessages();
 
 		// DELETE
-		logger.debug("deleting message " + messageEight.toString());
-		messageRepository.delete(messageEight);
-		printAllMessages();
-
-		// DELETE BY ID
-		logger.debug("deleting message with id=" + idFake);
-		messageRepository.deleteById(idFake);
-		printAllMessages();
-
-		Long idSeven = messageSeven.getId();
-		logger.debug("deleting message with id=" + idSeven);
-		messageRepository.deleteById(idSeven);
-		printAllMessages();
-
-		// DELETE ALL
-		List<Message> messageGroup = Arrays.asList(messageSix, messageFive);
-		logger.debug("deleting messages " + messageGroup);
-		messageRepository.deleteAll(messageGroup);
-		printAllMessages();
-
-		// DELETE ALL BY ID
-		List<Long> idGroup = Arrays.asList(messageFour.getId(), messageThree.getId());
-		logger.debug("deleting messages with ids " + idGroup);
-		messageRepository.deleteAllById(idGroup);
+		logger.info("deleting message " + messageEight);
+		messageService.delete(messageEight);
 		printAllMessages();
 
 		// FIND BY TEXT
-		logger.debug("finding all messages by text '" + messageOne.getText() + "'");
-		messageRepository.findByText(messageOne.getText()).forEach(m -> {
-			logger.debug("Message: " + m.toString());
-		});
-
-		logger.debug("finding all messages by text '" + messageTwo.getText() + "'");
-		messageRepository.findByText(messageTwo.getText()).forEach(m -> {
-			logger.debug("Message: " + m.toString());
+		Arrays.asList("fake text", messageOne.getText(), messageTwo.getText()).forEach(text -> {
+			logger.info("checking if message with text='" + text + "' exists...");
+			logger.info("Message with text='" + text + (
+					messageService.readByText(text)
+							.map(message -> "' found: " + message)
+							.orElse("' not found")));
 		});
 	}
 
 	private void printAllMessages() {
-		logger.debug("finding all messages...");
-		messageRepository.findAll().forEach(m -> {
-			logger.debug("Message: " + m.toString());
-		});
+		logger.info("finding all messages...");
+		messageService.readAll().forEach(m -> logger.info("Message: " + m));
 	}
 }
